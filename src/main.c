b2a337dbb24f324d36959bfe175722a7285d45db
lockedbyte <lockedbyte@gmail.com>
2021-04-20 15:23:41 +0200

    main fixes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
 
-    Protcheck 1.0.0 - @lockedbyte (https://github.com/lockedbyte/protcheck)
+    Protcheck 1.0.1 - @lockedbyte (https://github.com/lockedbyte/protcheck)
     
         A C utility to check an ELF binary protections parsing the ELF directly instead of using intermediate programs like readelf or grep.
         
@@ -22,10 +22,11 @@
 #include "protcheck_x86_64.h"
 #include "elf_defines.h"
 
-#define MAX_PATH_SIZE 256
+#ifndef MAX_PATH_SIZE
+	#define MAX_PATH_SIZE 4096
+#endif
 
-
-int precheck_bits(char *loaded) {
+int precheck_bits(void *loaded) {
 
     Elf32_Ehdr *a0 = (Elf32_Ehdr *)loaded;	
     Elf64_Ehdr *a1 = (Elf64_Ehdr *)loaded;
@@ -54,7 +55,7 @@ int is_directory(const char *path) {
 
 int main(int argc, char *argv[]) {
 
-    const char *memblock = NULL;
+    void *memblock = NULL;
     int fd = 0;
     struct stat sb;
     int global_size = 0;
@@ -64,14 +65,15 @@ int main(int argc, char *argv[]) {
     memset(file_path, '\0', sizeof(file_path));
     
     puts("\n\033[1;35m - [ ProtCheck ] -\033[0m\n ");
+    
     if(argc < 2) {
         printf("\033[0;93m[%%] Common usage: %s <file path>\n[%%] Remote system usage: %s <file path> <libc path>\n\n\033[0m", argv[0], argv[0]);
         exit(0);
     }
     
     if(strlen(argv[1]) < MAX_PATH_SIZE) {
-        strncpy(file_path, argv[1], MAX_PATH_SIZE - 2);
-        file_path[MAX_PATH_SIZE - 1] = '\0';
+        strncpy(file_path, argv[1], strlen(argv[1]));
+        file_path[strlen(argv[1])] = '\0';
     } else {
         printf("\033[1;31m[-] Max filepath size is %d bytes.\033[0m\n\n", MAX_PATH_SIZE);
         exit(0);
@@ -89,12 +91,15 @@ int main(int argc, char *argv[]) {
     }
     
     fstat(fd, &sb);
-
-    memblock = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-
+    
     global_size = sb.st_size;
 
-    if(memblock == MAP_FAILED) perror("mmap");
+    memblock = mmap(NULL, global_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+
+    if(memblock == MAP_FAILED) {
+    	printf("\033[1;31m[-] An unknown error ocurred.\033[0m\n\n");
+    	exit(1);
+    }
     
     if(precheck_bits(memblock) == ELFCLASS32) {
         if(argc > 2)
@@ -111,7 +116,7 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
         
-    printf("\n");
+    puts("");
     
     return 0;
     
